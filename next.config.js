18226b4fb5982f2615bb01a1fc809cfef199527e
Daniel Stephany <danielstephany85@gmail.com>
2025-03-20 11:24:55 -0700

    moved images to remote location

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'daniel-stephany-onwards-foodies-starting-project.s3.us-east-1.amazonaws.com',
+                port: '',
+                pathname: "/**"
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
